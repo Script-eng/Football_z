8169496470fc3c3fee072e7ccde9204fa3079752
Script-Eng <lesalonsteven@gmail.com>
2025-01-02 00:25:31 +0100

    specs controller routes

--- app/Http/Controllers/SpectatorController.php
+++ app/Http/Controllers/SpectatorController.php
@@ -13,4 +13,44 @@ class SpectatorController extends Controller
         $spectators = Spectator::all();
         return view('spectators.index', compact('spectators'));
     }
+    public function create()
+    {
+        return view('spectators.create');
+    }
+    public function store(Request $request)
+    {
+        $spectator = new Spectator();
+        $spectator->sname = $request->sname;
+        $spectator->male = $request->male;
+        $spectator->haspass = $request->haspass;
+        $spectator->save();
+
+        return redirect()->route('spectators.index')->with('success', 'Spectator created successfully!');
+    }
+    public function edit($id)
+    {
+        $spectator = Spectator::findOrFail($id);
+        return view('spectators.edit', compact('spectator'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $spectator = Spectator::findOrFail($id);
+        $spectator->sname = $request->sname;
+        $spectator->male = $request->male;
+        $spectator->haspass = $request->haspass;
+        $spectator->save();
+
+        return redirect()->route('spectators.index')->with('success', 'Spectator updated successfully!');
+    }
+
+    public function destroy($id)
+    {
+        $spectator = Spectator::findOrFail($id);
+        $spectator->delete();
+
+        return redirect()->route('spectators.index')->with('success', 'Spectator deleted successfully!');
+    }
+
+
 }
